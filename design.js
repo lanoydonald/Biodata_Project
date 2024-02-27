d2164322f8f047819e84bcca0ff2be994611e4e9
Lanoy_Donald <lanoydonald1@gmail.com>
2024-02-27 09:15:02 +0800

    Add 2 Features

---
+++ design.js
@@ -0,0 +1,9 @@
+function updateDateTime() {
+     var now =new Date();
+     var datetimeElement =
+document.getElementById('datetime');
+      datetimeElement.innerHTML = 'Current Date and Time:' + now.toLocaleString();
+      
+}
+updateDateTime();
+setInterval(updateDateTime, 1000);
